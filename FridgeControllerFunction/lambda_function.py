ebed004064ed56e96810841f72bc5653adda6a9a
nathanVanderGriend <168024505+nathanVanderGriend@users.noreply.github.com>
2024-12-02 14:13:27 -0800

    Update lambda_function.py
    I updated the lambda function under the fridge controller function.

--- FridgeControllerFunction/lambda_function.py
+++ FridgeControllerFunction/lambda_function.py
@@ -1,8 +1,39 @@
 import json
+import boto3
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+fridge_table = dynamodb.Table('FridgeInventory')
 
 def lambda_handler(event, context):
-    # TODO implement
+    #Searches for all items in Fridge table
+    response = fridge_table.scan()
+    items = response['Items']
+    
+    
+    try:
+        # Query for an ingredient in the FridgeInventory table and store it in results.
+        query_results = []
+        for ingredients in ingredients_list:
+            query_response = fridge_table.query(
+                KeyConditionExpression=boto3.dynamodb.conditions.Key('ingredient').eq(ingredient_to_query)
+        )
+        query_results.append(query_response['Items']) 
+        
+        # Handle items found in the query
+        if items:
+            print("Found ingredients:", items)
+        else:
+            print(f"No items found for {ingredient_to_query}")
+    #Catches exceptions during query
+    except Exception as e:
+            print(f"Error querying FridgeInventory table: {e}")
+            return {
+                'statusCode': 500,
+                'body': json.dumps(f"Error querying FridgeInventory table: {str(e)}")
+        }
+    #successful
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({'items': items})
     }
